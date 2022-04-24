e93fcf4bf08e6dc113eb3daddbde8f41c926b8be
Stephen Cherry <stephen.m.cherry91@gmail.com>
2022-04-24 12:01:53 -0400

    datetime to utc and seperated config to util

--- app.py
+++ app.py
@@ -5,27 +5,28 @@ Main App File
 from datetime import datetime
 import logging
 import os
-from dotenv import dotenv_values
 import discord
 from music_player import MusicPlayer
 
 from utils.birthday import daily_birthday_jobs, set_run_time
+from utils.config import get_config
 
-# Load configuration from .env
-config = dotenv_values(".env")
+config = get_config()
+
+GUILD_ID = int(config.get("GUILD_ID"))
 
 # Configure Logging
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(
-    filename=f"logs/discord-{datetime.now()}.log", encoding="utf-8", mode="w"
+    filename=f"logs/discord-{datetime.utcnow()}.log", encoding="utf-8", mode="w"
 )
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
 )
 logger.addHandler(handler)
 
-bot = discord.Bot(debug_guilds=[785658574474969088], intents=discord.Intents.all())
+bot = discord.Bot(debug_guilds=[GUILD_ID], intents=discord.Intents.all())
 music_player = MusicPlayer(bot)
 
 for filename in os.listdir("./cogs"):
@@ -42,7 +43,7 @@ async def on_ready():
     # Sets initial runtime for birthday jobs
     # Birthday jobs run at 10am
     # Runs same day if before 10am
-    if datetime.now().hour > 9:
+    if datetime.utcnow().hour > 14:
         bot.birthday_job_runtime = set_run_time(1)
     else:
         bot.birthday_job_runtime = set_run_time()
